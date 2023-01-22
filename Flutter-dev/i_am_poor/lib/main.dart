381df906b23926e3a34fb3bc091acc9ebf1c2571
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-01-22 23:15:15 +0530

    I am Poor app

--- Flutter-dev/i_am_poor/lib/main.dart
+++ Flutter-dev/i_am_poor/lib/main.dart
@@ -3,10 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-        appBar: AppBar(
-      title: const Center(
-        child: Text('I am Poor'),
+      appBar: AppBar(
+        title: const Center(
+          child: Text('An app With Aesthetic Image'),
+        ),
+        backgroundColor: Colors.deepPurple.shade200,
       ),
-    )),
+      body: Center(
+        child: Image.asset('images/home_img.png'),
+      ),
+    ),
   ));
 }
