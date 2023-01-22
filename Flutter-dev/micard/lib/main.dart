284842050f074cd85f5e1dc44f5a775777cad62f
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-01-23 00:00:55 +0530

    learnt about containers

--- Flutter-dev/micard/lib/main.dart
+++ Flutter-dev/micard/lib/main.dart
@@ -1,5 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.blue,
+          body: SafeArea(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(30, 10, 50, 20),
+              height: 100.0,
+              width: 100.0,
+              color: Colors.white,
+              child: Center(child: const Text('Child widget')),
+            ),
+          )),
+    );
+  }
 }
