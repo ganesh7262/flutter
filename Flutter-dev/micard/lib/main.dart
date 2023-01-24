cf659dc62457354dfa0e7b06c4333d2fac0b50fb
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-01-24 19:59:36 +0530

    final touches
    1->centered all the widgets
    2->used listtile
    3->final touches

--- Flutter-dev/micard/lib/main.dart
+++ Flutter-dev/micard/lib/main.dart
@@ -14,7 +14,7 @@ class MyWidget extends StatelessWidget {
           backgroundColor: Colors.indigo.shade100,
           body: SafeArea(
               child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               const SizedBox(
                 width: double.infinity,
@@ -32,6 +32,9 @@ class MyWidget extends StatelessWidget {
                     fontStyle: FontStyle.normal,
                     fontWeight: FontWeight.bold),
               ),
+              const SizedBox(
+                height: 10.0,
+              ),
               const Text(
                 'FLUTTER DEVLOPER',
                 style: TextStyle(
@@ -41,69 +44,56 @@ class MyWidget extends StatelessWidget {
                     fontStyle: FontStyle.normal,
                     fontWeight: FontWeight.bold),
               ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 0),
-                child: Card(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
-                  color: Colors.indigo.shade200,
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Row(children: const [
-                      Icon(
+              const SizedBox(
+                height: 20.0,
+                child: Divider(
+                  thickness: 2,
+                  indent: 120,
+                  endIndent: 120,
+                  color: Color.fromARGB(255, 123, 132, 183),
+                ),
+              ),
+              Card(
+                margin:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+                color: Colors.indigo.shade200,
+                child: const Padding(
+                    padding: EdgeInsets.all(4.0),
+                    child: ListTile(
+                      dense: true,
+                      leading: Icon(
                         Icons.phone,
                         size: 30,
                       ),
-                      Text(
-                        ' :',
-                        style: TextStyle(fontSize: 25),
-                      ),
-                      SizedBox(
-                        width: 40,
-                      ),
-                      Text(
+                      title: Text(
                         '+91 1234567891',
                         style: TextStyle(
                           letterSpacing: 3,
                           fontFamily: 'SourceSansPro',
                           fontSize: 18,
                         ),
-                      )
-                    ]),
-                  ),
-                ),
+                      ),
+                    )),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-                child: Card(
-                  margin:
-                      const EdgeInsets.symmetric(vertical: 0, horizontal: 30),
-                  color: Colors.indigo.shade200,
-                  child: Padding(
-                    padding: const EdgeInsets.all(5.0),
-                    child: Row(children: const [
-                      Icon(
+              Card(
+                margin: const EdgeInsets.symmetric(vertical: 0, horizontal: 30),
+                color: Colors.indigo.shade200,
+                child: const Padding(
+                  padding: EdgeInsets.all(5.0),
+                  child: ListTile(
+                      dense: true,
+                      leading: Icon(
                         Icons.email,
                         size: 30,
                       ),
-                      Text(
-                        ' :',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      SizedBox(
-                        width: 12,
-                      ),
-                      Text(
+                      title: Text(
                         'ganeshshinde72620@gmail.com',
                         style: TextStyle(
                           letterSpacing: 1,
                           fontFamily: 'SourceSansPro',
                           fontSize: 17,
                         ),
-                      )
-                    ]),
-                  ),
+                      )),
                 ),
               )
             ],
