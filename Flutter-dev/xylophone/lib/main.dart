814996617f22826bdba836ea77114620017ed60f
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-01-28 12:44:16 +0530

    packages
    loaded the music sounds
    updated pubspec
    learnt how to uses packages

--- Flutter-dev/xylophone/lib/main.dart
+++ Flutter-dev/xylophone/lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(),
-    ),
-  );
+  runApp(const Xylophone());
+}
+
+class Xylophone extends StatelessWidget {
+  const Xylophone({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Simple Music app'),
+        ),
+        body: SafeArea(child: Text(nouns.first)),
+      ),
+    );
+  }
 }
