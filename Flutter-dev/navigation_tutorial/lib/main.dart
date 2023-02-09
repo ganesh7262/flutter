dec9d980d4a952763e651d645d958c9e86d59aef
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-02-09 18:09:22 +0530

    Navigation in flutter
    how to go to different pages and screens in flutter

---
+++ Flutter-dev/navigation_tutorial/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:navigation_tutorial/screen1.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Screen1(),
+    ),
+  );
+}
---
+++ Flutter-dev/navigation_tutorial/lib/screen1.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Screen1 extends StatelessWidget {
+  const Screen1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: const Center(
+          child: Text('Screen1'),
+        ),
+      ),
+    );
+  }
+}
