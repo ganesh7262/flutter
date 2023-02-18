1643a3a11cd3f531ab9d1fbdc4a692120a7339df
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-02-18 23:47:40 +0530

    API

--- Flutter-dev/clima/lib/screens/loading_screen.dart
+++ Flutter-dev/clima/lib/screens/loading_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,12 +9,30 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  String apiKey = 'b7bbcc47b267a503b91b217e48b0a950';
+  double? latitude;
+  double? longitude;
+
   @override
   void getLocation() async {
     Location pos = Location();
     await pos.getCurrentLoaction();
-    print(pos.latitude);
-    print(pos.longitude);
+    latitude = pos.latitude;
+    longitude = pos.longitude;
+    print(latitude);
+    print(longitude);
+    GetData();
+  }
+
+  void GetData() async {
+    http.Response data = await http.get(
+      Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey'),
+    );
+    var dataFromRes = jsonDecode(
+      data.body.toString(),
+    );
+    print(dataFromRes);
   }
 
   @override
@@ -20,6 +40,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
     // TODO: implement initState
     super.initState();
     getLocation();
+    GetData();
   }
 
   @override
