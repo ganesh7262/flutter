657de95a73872901f6b43dc6f5e934ee527be432
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-06-16 23:16:00 +0530

    quiz page

--- Flutter-dev/advquiz/lib/screens/quiz_screen.dart
+++ Flutter-dev/advquiz/lib/screens/quiz_screen.dart
@@ -11,7 +11,37 @@ class _QuizState extends State<Quiz> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("object")),
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 172, 22, 87),
+            Color.fromARGB(255, 202, 13, 123),
+            Color.fromARGB(255, 202, 13, 123),
+            Color.fromARGB(255, 202, 13, 123),
+            Color.fromARGB(255, 202, 13, 123),
+            Color.fromARGB(255, 172, 22, 87),
+          ],
+        )),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: double.infinity,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text(
+                "Here resides the Question",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
