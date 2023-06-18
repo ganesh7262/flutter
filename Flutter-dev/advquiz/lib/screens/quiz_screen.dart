e3b84ebd0d8bbebe2fafc813b2388fc18aec799c
Ganesh Shinde <ganeshshinde72620@gmail.com>
2023-06-18 10:27:32 +0530

    Update quiz_screen.dart

--- Flutter-dev/advquiz/lib/screens/quiz_screen.dart
+++ Flutter-dev/advquiz/lib/screens/quiz_screen.dart
@@ -8,6 +8,15 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  String op1 = "option 1 hello";
+  String op2 = "Option 1 hello world";
+  String ques =
+      "Here resides the Question hello world sentinels on the top lets go baby kang kang";
+
+  void qUpdate() {
+    op1 = op2;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,20 +36,54 @@ class _QuizState extends State<Quiz> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(
+            const SizedBox(
               width: double.infinity,
             ),
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                "Here resides the Question",
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            )
+            questionBox(ques),
+            optionButton(op1),
+            optionButton(op1),
+            optionButton(op1),
+            optionButton(op1),
           ],
         ),
+      ),
+    );
+  }
+
+  Padding questionBox(String ques) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: const Color.fromARGB(197, 135, 21, 91),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 15),
+          child: Text(
+            ques,
+            style: const TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Padding optionButton(String op) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+      child: FilledButton(
+        onPressed: () {
+          setState(() {
+            qUpdate();
+          });
+        },
+        style: FilledButton.styleFrom(
+          backgroundColor: const Color.fromARGB(197, 135, 21, 91),
+          minimumSize: const Size.fromHeight(45),
+        ),
+        child: Text(op),
       ),
     );
   }
